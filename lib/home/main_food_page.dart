55fbbeb871025aba4eddbb50efd878295f211859
phamhuuloc219 <phamhuuloc2192003@gmail.com>
2025-01-02 23:56:54 +0700

    feat: coding home page

---
+++ lib/home/main_food_page.dart
@@ -0,0 +1,50 @@
+import 'package:e_commerce_app/utils/colors.dart';
+import 'package:e_commerce_app/widgets/big_text.dart';
+import 'package:flutter/material.dart';
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({super.key});
+
+  @override
+  State<MainFoodPage> createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top:45, bottom: 15),
+            padding: EdgeInsets.only(left: 20, right: 20),
+            child: Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      BigText(text: "Vietnam"),
+                      Text("City"),
+                    ],
+                  ),
+                  Center(
+                    child: Container(
+                      width: 45,
+                      height: 45,
+                      child: Icon(Icons.search, color: Colors.white,),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: AppColors.mainColor,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
