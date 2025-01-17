548e63e66f73dbbe2b46235ff6d59e77c2b734d1
phamhuuloc219 <phamhuuloc2192003@gmail.com>
2025-01-17 23:40:11 +0700

    feat: build stack container

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,3 +1,7 @@
+import 'package:e_commerce_app/utils/colors.dart';
+import 'package:e_commerce_app/widgets/big_text.dart';
+import 'package:e_commerce_app/widgets/icon_and_text_widget.dart';
+import 'package:e_commerce_app/widgets/small_text.dart';
 import 'package:flutter/material.dart';
 
 class FoodPageBody extends StatefulWidget {
@@ -8,11 +12,14 @@ class FoodPageBody extends StatefulWidget {
 }
 
 class _FoodPageBodyState extends State<FoodPageBody> {
+  PageController pageController = PageController(viewportFraction: 0.9);
   @override
   Widget build(BuildContext context) {
     return Container(
+      color: Colors.redAccent,
       height: 320,
       child: PageView.builder(
+        controller: pageController,
         itemCount: 5,
         itemBuilder: (context, position) {
           return _buildPageItem(position);
@@ -20,14 +27,79 @@ class _FoodPageBodyState extends State<FoodPageBody> {
     );
   }
   Widget _buildPageItem(int index){
-    return Container(
-      height: 220,
-      margin: EdgeInsets.only(left: 5, right: 5),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(30),
-        color: index.isEven?Color(0xFF69c5df): Color(0xFF9294cc),
-        image: DecorationImage(image: AssetImage("assets/image/food1.png"))
-      ),
+    return Stack(
+      children: [
+        Container(
+          height: 220,
+          margin: EdgeInsets.only(left: 10, right: 10),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: index.isEven?Color(0xFF69c5df): Color(0xFF9294cc),
+              image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage("assets/image/food0.png"))
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 120,
+            margin: EdgeInsets.only(left: 30, right: 30, bottom: 30),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.white
+            ),
+            child: Container(
+              padding: EdgeInsets.only(top: 15, left: 15, right: 15),
+              child: Column(
+                children: [
+                  BigText(text: "chinese side"),
+                  SizedBox(height: 10,),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Wrap(
+                        children: List.generate(
+                            5, (index) {
+                          return Icon(
+                            Icons.star, color: AppColors.mainColor, size: 15,);
+                          },
+                        ),
+                      ),
+                      SizedBox(width: 10,),
+                      SmallText(text: "4.5"),
+                      SizedBox(width: 10,),
+                      SmallText(text: "1287"),
+                      SizedBox(width: 10,),
+                      SmallText(text: "comments")
+                    ],
+                  ),
+                  SizedBox(height: 20,),
+                  Row(
+                    children: [
+                      IconAndTextWidget(
+                          icon: Icons.circle_sharp,
+                          text: "Normal",
+                          iconColor: AppColors.iconColor1
+                      ),
+                      IconAndTextWidget(
+                          icon: Icons.location_on,
+                          text: "1.7km",
+                          iconColor: AppColors.mainColor
+                      ),
+                      IconAndTextWidget(
+                          icon: Icons.access_time_rounded,
+                          text: "32min",
+                          iconColor: AppColors.iconColor2
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
